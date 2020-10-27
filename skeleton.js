e0876154e7fa69a90f274ceaaec967eca059aacc
shizhikai <shizhikai@pensees.ai>
2020-10-27 20:47:04 +0800

    代码

--- skeleton.js
+++ skeleton.js
@@ -1,38 +1,42 @@
+
 (async() => {
     const puppeteer = require('puppeteer')
     const fs = require('fs')
-    const webUrl = 'http://localhost:8080/#/'
+    const webUrl = 'http://127.0.0.1:8080/#/'
     const selectorJs = './src/skeletonJs.js'
     // 启动浏览器
     const browser = await puppeteer.launch()
-    const buildFindRootEle = '';
+    const buildFindRootEle = '#app';
     const buildUseStartRootEle = '';
     const buildUseEndRootEle = ''
-    const buildFileUrl = ''
-    const buildFileSaveUrl = ''
+    const buildFileUrl = './dist/index.html'
+    const buildFileSaveUrl = './dist/index2.html'
     // 新建tab页面
     const page = await browser.newPage()
     // 打开网站地址
     await page.goto(webUrl)
     //插入骨架屏js
     await page.addScriptTag({
-       url:selectorJs
+       path:selectorJs
     })
-    await page.waitFor(2000)
+    await page.waitForTimeout(2000)
     const skeletonStr = await page.evaluate(async (buildFindRootEle) => {
         window.skeleton()
         let str = document.querySelector(buildFindRootEle).innerHTML
         return str
     },buildFindRootEle)
-    const addSkeletonStr = buildUseStartRootEle + skeletonStr + buildUseEndRootEle
+    console.log('生成骨架屏结构')
+    // console.log(skeletonStr)
+    // const addSkeletonStr = buildUseStartRootEle + skeletonStr + buildUseEndRootEle
+    // console.log(addSkeletonStr)
     //读取文件
     fs.readFile(buildFileUrl,'utf8',async (err,data) => {
         if(err) {
             console.log(err)
             return false
         }
-        let reg = '/<body>/'
-        const newContent = data.replace(reg,`<body>${addSkeletonStr}`)
+        let reg = /<body>/;
+        const newContent = data.replace(reg,'<body>'+ skeletonStr)
         fs.writeFile(buildFileSaveUrl,newContent,'utf8',async(err)=>{
             if(err) {
                 console.log(err)
