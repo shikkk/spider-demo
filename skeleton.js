891a6a3cb0634f78f7b4c147339ff9721005e929
shizhikai <shizhikai@pensees.ai>
2020-10-28 20:37:53 +0800

    代码

--- skeleton.js
+++ skeleton.js
@@ -5,12 +5,17 @@
     const webUrl = 'http://127.0.0.1:8080/#/'
     const selectorJs = './src/skeletonJs.js'
     // 启动浏览器
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch(
+      {
+        headless: false,
+        defaultViewport: { width: 1440, height: 1000 },
+        }
+    )
     const buildFindRootEle = '#app';
     const buildUseStartRootEle = '';
     const buildUseEndRootEle = ''
-    const buildFileUrl = './dist/index.html'
-    const buildFileSaveUrl = './dist/index2.html'
+    const buildFileUrl = './web-demo/dist/index.html'
+    const buildFileSaveUrl = './web-demo/dist/index2.html'
     // 新建tab页面
     const page = await browser.newPage()
     // 打开网站地址
@@ -27,8 +32,6 @@
     },buildFindRootEle)
     console.log('生成骨架屏结构')
     // console.log(skeletonStr)
-    // const addSkeletonStr = buildUseStartRootEle + skeletonStr + buildUseEndRootEle
-    // console.log(addSkeletonStr)
     //读取文件
     fs.readFile(buildFileUrl,'utf8',async (err,data) => {
         if(err) {
@@ -37,15 +40,14 @@
         }
         let reg = /<body>/;
         const newContent = data.replace(reg,'<body>'+ skeletonStr)
-        fs.writeFile(buildFileSaveUrl,newContent,'utf8',async(err)=>{
+        fs.writeFile(buildFileUrl,newContent,'utf8',async(err)=>{
             if(err) {
                 console.log(err)
                 return false
             }
-            //关闭无界面浏览器
-            await browser.close()
-
             console.log('构建完毕！结束子进程')
+            // //关闭无界面浏览器
+            // await browser.close()
         })
 
     })
--- src/skeletonJs.js
+++ src/skeletonJs.js
@@ -1,6 +1,6 @@
 window.skeleton = function(){
-  var box1 = document.querySelectorAll('*[skeleton="0"],*[skeleton="1"],*[skeleton="4"],*[skeleton="3"]')
-  for(var i = 0;i<box1.length;i++){
+  let box1 = document.querySelectorAll('*[skeleton="0"],*[skeleton="1"],*[skeleton="4"],*[skeleton="3"]')
+  for(let i = 0;i<box1.length;i++){
     box1[i].innerHTML = ''
     box1[i].style.background = '#ccc'
     box1[i].style.borderRadius = '5px'
